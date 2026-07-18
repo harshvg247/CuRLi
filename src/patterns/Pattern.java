197cb1c5f9bc219b909fd064c60e03e46b52edd2
Harsh Vardhan Gupta <harsh247gupta@gmail.com>
2026-07-19 00:22:30 +0530

    Revert "add interface definition as comment"
    This reverts commit 72eb916d726ad2db6429e25a262e7380fd2fa733.

--- src/patterns/Pattern.java
+++ src/patterns/Pattern.java
@@ -1,11 +1,5 @@
 package patterns;
 
-//Interface:
-//No Instantiation: You cannot create an object directly from an interface.
-//Implicit Modifiers: Interface variables are automatically public static final (constants).
-//Abstract Methods: By default, methods lack a body and are implicitly public abstract.
-//No Constructors: Interfaces cannot contain constructor methods because they cannot be initialized directly.
-
 public interface Pattern {
     double integrate(long t0, long t1);
 
